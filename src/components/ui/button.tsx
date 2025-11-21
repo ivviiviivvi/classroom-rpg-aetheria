49ac001bd8a52b7afbd408fb7c3d880551b8298b
_]| |{::::4_ivi374::}~,.'; • ;'.,~{::4_8100ivi::::}|| [___ <etceter4@etceter4.com>
2025-11-21 21:50:00 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -35,7 +35,7 @@ const buttonVariants = cva(
   }
 )
 
-function Button({
+const Button = ({
   className,
   variant,
   size,
@@ -44,7 +44,7 @@ function Button({
 }: ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
-  }) {
+  }) => {
   const Comp = asChild ? Slot : "button"
 
   return (
@@ -55,5 +55,7 @@ function Button({
     />
   )
 }
+
+Button.displayName = "Button"
 
 export { Button, buttonVariants }
